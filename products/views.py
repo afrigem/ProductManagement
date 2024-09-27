786f4bf5d7aa87882dcf151e0fb37a03d6bb482e
torddey <ea.torddey@gmail.com>
2024-09-27 00:26:09 +0000

    Update product details

--- products/views.py
+++ products/views.py
@@ -3,7 +3,9 @@ from .models import Product
 
 def detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
+    related_products = Product.objects.filter(category=product.category).exclude(pk=pk)[0:3]
 
     return render(request, 'product/detail.html', {
-        'product': product
+        'product': product,
+        'related_products': related_products
     })
