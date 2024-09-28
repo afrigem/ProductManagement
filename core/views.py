db60df07962a8fa7719333f9b0bfb6c4a23702f8
torddey <ea.torddey@gmail.com>
2024-09-28 00:42:00 +0000

    Update Signup

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from products.models import Category, Product
 from .forms import SignupForm
 
@@ -15,7 +15,17 @@ def contact (request):
     return render(request, 'core/contact.html',)
 
 def signup(request):
-    form = SignupForm()
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+
+            return redirect('/login/')
+    else:
+
+        form = SignupForm()
+
     return render(request, 'core/signup.html', {
         'form': form
     })
